6864d85478938286250e97c6ad19af985809e93d
Abel Cheng <abelcys@gmail.com>
2020-04-12 13:15:02 -0400

    preparing sample server

--- Sample/PyWebApi.IIS/app.py
+++ Sample/PyWebApi.IIS/app.py
@@ -20,20 +20,11 @@ def wsgi_app():
     return bottle.default_app()
 
 if __name__ == '__main__':
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '8080'))
     except ValueError:
-        PORT = 5555
-
-    @bottle.route('/static/<filepath:path>')
-    def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
+        PORT = 8080
 
     # Starts a local test server.
     bottle.run(server='wsgiref', host=HOST, port=PORT)
--- Sample/PyWebApi.IIS/routes.py
+++ Sample/PyWebApi.IIS/routes.py
@@ -2,37 +2,48 @@
     Routes for the sample PyWebApi Service.
 """
 
+import os
 from bottle import route, request, abort
 from pywebapi import execute, cors
+
+
+_user_script_root = os.getenv("USER_SCRIPT_ROOT")
+if not os.path.isabs(_user_script_root):
+    _user_script_root = os.path.join(os.path.abspath(os.path.dirname(__file__)), _user_script_root)
+_user_script_root = os.path.normpath(_user_script_root)
+
+
+def _get_user() -> str:
+    return request.auth[0] if request.auth else None
 
 
 def authorize(func):
 
     def wrapped(*args, **kwargs):
-        if not request.auth or not request.auth[0]:
+        if not _get_user():
             abort(401, "The requested resource requires user authentication.")
         return func(*args, **kwargs)
 
     return wrapped
 
 
-@route('/whoami')
+@route(path='/whoami', method=['GET', 'POST'])
 @authorize
 def who_am_i():
-    return request.auth[0]
+    return _get_user()
 
 
-def check_permission(app:str, user:str, module_func:str)->bool:
+def check_permission(app:str, user:str, module_func:str) -> bool:
     #TODO: add your implementation of permission checks
     return True
 
 
-@route('/pyscripts/<app:str>/<path:path>')
+@route(path='/pyscripts/<app>/<funcpath:path>', method=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 @authorize
-def execute_module_level_function(app:str, path:str):
-    user_name = request.auth[0]
+def execute_module_level_function(app:str, funcpath:str):
+    user_name = _get_user()
 
-    if check_permission(app, request.auth[0], path):
-        return execute('d:\\user-script-root\\', request, {'actual_username': user_name})
+    if check_permission(app, user_name, funcpath):
+        return execute(_user_script_root, request, {'actual_username': user_name})
     else:
-        abort(401, f"Current user ({repr(user_name)}) does not have permission to execute the requested {repr(path)}.")
+        abort(401, f"Current user ({repr(user_name)}) does not have permission to execute the requested {repr(funcpath)}.")
