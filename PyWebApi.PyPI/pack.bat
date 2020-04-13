1250f8961187f9b0217695de73b90f775b5188c1
Abel Cheng <abelcys@gmail.com>
2020-04-12 23:08:22 -0400

    Updated pack.bat

--- PyWebApi.PyPI/pack.bat
+++ PyWebApi.PyPI/pack.bat
@@ -5,4 +5,7 @@ SET EXISTS_IN_PATH=0
 FOR /F "delims=" %%i IN ('PATH ^| Find /C /I "%THIS_SCRIPT_PATH%"') DO SET EXISTS_IN_PATH=%%i 2>NUL
 If %EXISTS_IN_PATH%==0 SET "PATH=%THIS_SCRIPT_PATH%;%PATH%"
 
+Del /q build\lib\pywebapi\*
+Del /q dist\*
+
 Python.exe setup.py bdist_wheel --universal
