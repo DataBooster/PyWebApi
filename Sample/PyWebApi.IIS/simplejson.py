5b6762582f5558e0dd79ff62c30f820e793a8a61
Abel Cheng <abelcys@gmail.com>
2020-04-23 22:32:15 -0400

    Addded python-dateutil into PyWebApi.IIS

--- Sample/PyWebApi.IIS/simplejson.py
+++ Sample/PyWebApi.IIS/simplejson.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from jsonpickle import decode, encode
+from jsonpickle import encode
+from json import loads as decode
+from dateutil import parser
 
 
 def dumps(obj, **kwargs):
@@ -7,5 +9,22 @@ def dumps(obj, **kwargs):
     return encode(obj, **kwargs)
 
 
+def _json_datetime_decode_hook(pairs):
+    obj_dict = {}
+
+    for k, v in pairs:
+        if isinstance(v, str) and 10 <= len(v) <= 50:
+            try:
+                obj_dict[k] = dt_parser.parse(v)
+            except: #ValueError:
+                obj_dict[k] = v
+        else:
+            obj_dict[k] = v
+
+    return obj_dict
+
+
+
 def loads(s, **kwargs):
+    kwargs['object_pairs_hook'] = kwargs.get('object_pairs_hook', _json_datetime_decode_hook)
     return decode(s, **kwargs)
