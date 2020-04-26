d73ddbb8a58fe9fea271947a59e836cc14045b72
Abel Cheng <abelcys@gmail.com>
2020-04-25 22:50:12 -0400

    Removed query.py from MdxReader.pyproj

--- Sample/UserApps/MdxReader/query.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from adomd_client import AdomdClient
-
-
-def query(connection_string:str, command_text:str, result_model:str='ListOfDict', column_mapping:dict={}):
-    with AdomdClient(connection_string) as client:
-        return client.query(command_text, result_model, column_mapping)
