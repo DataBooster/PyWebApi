afa55d734ac2434a0479a42c1ec80363a4d76f4a
Abel Cheng <abelcys@gmail.com>
2020-08-25 00:10:58 -0400

    Updated publish-samples.bat

--- Sample/publish-samples.bat
+++ Sample/publish-samples.bat
@@ -6,7 +6,10 @@ IF NOT EXIST "%DESTINATION%" GOTO USAGE
 
 CD /d "%~dp0"
 RoboCopy . "%DESTINATION%" /s /xf *.user publish-samples.bat /xd __pycache__ *env obj bin samples .vs
+
+IF NOT EXIST "%DESTINATION%\PyWebApi.IIS\log" MKDIR "%DESTINATION%\PyWebApi.IIS\log"
 Copy /y nul "%DESTINATION%\PyWebApi.IIS\log\wfastcgi.log" > nul
+
 Goto:eof
 
 :USAGE
